b41cefdaee30e75586fe20d303624c08c64189b2
A Sulthan Batzha <sulthanbatzha1232@gmail.com>
2025-02-24 03:46:00 -0800

    Add files via upload

---
+++ lib/app/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const lightGreens = Color(0xff09b5aa);
+const lightblacks = Color(0xff141414);
+
+const blue = Color.fromARGB(255, 32, 67, 224);
+const darkBlue = Color(0xff3442af);
+const bgcolor = Color(0xffe4ecfa);
+const lighgrey = Color(0xffb8c6e6);
+
+
+   // #09b5aa      #141414
